ac609fdbbe2fa593836ac449eed3c0ce4ad036fe
kane <m.hamaskhan@gmail.com>
2022-03-15 00:26:08 +0500

    run modified 1

---
+++ run.py
@@ -0,0 +1,92 @@
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib
+import matplotlib.pyplot as plt
+from keras import Sequential
+from keras.callbacks import TensorBoard
+from keras.layers import Input
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import Conv3D, Deconv3D
+from keras.layers.core import Activation
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras.optimizers import Adam
+import numpy as np
+import scipy.io as io
+import scipy.ndimage as nd
+import tensorflow as tf
+import glob
+import os
+import time
+
+def build_discriminator():
+    """
+    Create a Discriminator Model using hyperparameters values defined as follows
+    """
+
+    dis_input_shape = (64, 64, 64, 1)
+    dis_filters = [64, 128, 256, 512, 1]
+    dis_kernel_sizes = [4, 4, 4, 4, 4]
+    dis_strides = [2, 2, 2, 2, 1]
+    dis_paddings = ['same', 'same', 'same', 'same', 'valid']
+    dis_alphas = [0.2, 0.2, 0.2, 0.2, 0.2]
+    dis_activations = ['leaky_relu', 'leaky_relu', 'leaky_relu',
+                       'leaky_relu', 'sigmoid']
+    dis_convolutional_blocks = 5
+
+    dis_input_layer = Input(shape=dis_input_shape)
+
+    # The first 3D Convolutional block
+    a = Conv3D(filters=dis_filters[0],
+               kernel_size=dis_kernel_sizes[0],
+               strides=dis_strides[0],
+               padding=dis_paddings[0])(dis_input_layer)
+    # a = BatchNormalization()(a, training=True)
+    a = LeakyReLU(dis_alphas[0])(a)
+
+    # Next 4 3D Convolutional Blocks
+    for i in range(dis_convolutional_blocks - 1):
+        a = Conv3D(filters=dis_filters[i + 1],
+                   kernel_size=dis_kernel_sizes[i + 1],
+                   strides=dis_strides[i + 1],
+                   padding=dis_paddings[i + 1])(a)
+        a = BatchNormalization()(a, training=True)
+        if dis_activations[i + 1] == 'leaky_relu':
+            a = LeakyReLU(dis_alphas[i + 1])(a)
+        elif dis_activations[i + 1] == 'sigmoid':
+            a = Activation(activation='sigmoid')(a)
+
+    dis_model = Model(inputs=[dis_input_layer], outputs=[a])
+    return dis_model
+
+    def build_generator():
+    """
+    Create a Generator Model with hyperparameters values defined as follows
+    """
+    z_size = 200
+    gen_filters = [512, 256, 128, 64, 1]
+    gen_kernel_sizes = [4, 4, 4, 4, 4]
+    gen_strides = [1, 2, 2, 2, 2]
+    gen_input_shape = (1, 1, 1, z_size)
+    gen_activations = ['relu', 'relu', 'relu', 'relu', 'sigmoid']
+    gen_convolutional_blocks = 5
+
+    input_layer = Input(shape=gen_input_shape)
+
+    # First 3D transpose convolution(or 3D deconvolution) block
+    a = Deconv3D(filters=gen_filters[0],
+                 kernel_size=gen_kernel_sizes[0],
+                 strides=gen_strides[0])(input_layer)
+    a = BatchNormalization()(a, training=True)
+    a = Activation(activation='relu')(a)
+
+    # Next 4 3D transpose convolution(or 3D deconvolution) blocks
+    for i in range(gen_convolutional_blocks - 1):
+        a = Deconv3D(filters=gen_filters[i + 1],
+                     kernel_size=gen_kernel_sizes[i + 1],
+                     strides=gen_strides[i + 1], padding='same')(a)
+        a = BatchNormalization()(a, training=True)
+        a = Activation(activation=gen_activations[i + 1])(a)
+
+    gen_model = Model(inputs=[input_layer], outputs=[a])
+    return gen_model
